cb8e63f7794e213c2c2099a8a140cdfeeab8967a
psycoder <parksang1993@gmail.com>
2023-11-27 14:42:20 +0900

    Initial Commit

---
+++ Tutorial-Recording Audio/Tutorial_Recording_AudioApp.swift
@@ -0,0 +1,21 @@
+//
+//  Tutorial_Recording_AudioApp.swift
+//  Tutorial-Recording Audio
+//
+//  Created by 박상욱 on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial_Recording_AudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
